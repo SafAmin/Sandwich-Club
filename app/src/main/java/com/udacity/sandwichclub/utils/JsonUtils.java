d5e564a339cfeeafffbff36026d3c23aa87bcc42
Safa Amin <safa2amin@gmail.com>
2018-02-24 14:26:24 +0200

    Parsed JSON string inside `JsonUtils`

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,52 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class JsonUtils {
 
+    private static Sandwich sandwichModel;
+
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+
+        try {
+            JSONObject mainJSONObject = new JSONObject(json);
+            JSONObject nameObject = mainJSONObject.getJSONObject("name");
+            String name = nameObject.getString("mainName");
+            JSONArray alsoKnownAs = nameObject.getJSONArray("alsoKnownAs");
+            ArrayList<String> alsoKnownAsList = convertJSONArrayToArrayList(alsoKnownAs);
+            String placeOfOrigin = mainJSONObject.getString("placeOfOrigin");
+            String description = mainJSONObject.getString("description");
+            String image = mainJSONObject.getString("image");
+            JSONArray ingredients = mainJSONObject.getJSONArray("ingredients");
+            ArrayList<String> ingredientsList = convertJSONArrayToArrayList(ingredients);
+
+            sandwichModel = new Sandwich(name,alsoKnownAsList, placeOfOrigin, description, image,
+                    ingredientsList);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return sandwichModel;
+    }
+
+    private static ArrayList<String> convertJSONArrayToArrayList(JSONArray jsonArray) {
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        if (jsonArray != null) {
+            for (int i=0;i<jsonArray.length();i++){
+                try {
+                    arrayList.add(jsonArray.getString(i));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return arrayList;
     }
 }
